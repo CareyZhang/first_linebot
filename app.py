04ad50000f8d6dcfe3daf1d106ee13fb8672a1c9
CareyZhang <loves29955173@gmail.com>
2019-06-13 15:37:49 +0800

    msg detail

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import json
 from flask import Flask, request, abort
 
 from linebot import (
@@ -34,7 +33,6 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-	print(event)
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
