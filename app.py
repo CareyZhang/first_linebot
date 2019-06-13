53a77347d198720db2aadc56949ba7c1ee7003ab
CareyZhang <loves29955173@gmail.com>
2019-06-13 16:23:31 +0800

    msg detail

--- app.py
+++ app.py
@@ -35,7 +35,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     try:
-        message = TextSendMessage(text=type(event.message.text))
+        message = TextSendMessage(text=event.message.text)
+        print(type(event.message.text))
         line_bot_api.reply_message(event.reply_token, message)
     except:
         message = TextSendMessage(text=type(event.message.text))
