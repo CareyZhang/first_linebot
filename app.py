0f4e2b21c8976aedd60745eed00e95fd46521986
CareyZhang <loves29955173@gmail.com>
2019-06-14 15:14:18 +0800

    resume test

--- app.py
+++ app.py
@@ -87,16 +87,13 @@ def handle_message(event):
 
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker_message(event):
-"""
     line_bot_api.reply_message(
         event.reply_token,
         StickerSendMessage(
             package_id=event.message.package_id,
             sticker_id=event.message.sticker_id)
     )
-    message = TextSendMessage(text=str(event.message.package_id)+'-'.str(event.message.sticker_id))
-"""
-    message = TextSendMessage(text='test')
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
     
 @handler.add(MessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
