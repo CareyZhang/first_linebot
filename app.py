4c7802959245e871820608b87c96fcd376b78da9
CareyZhang <loves29955173@gmail.com>
2019-06-14 10:13:13 +0800

    Create rich menu

--- app.py
+++ app.py
@@ -18,15 +18,28 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('depzJAsLeVp71tO5p2owx5eqjdNUEBcDfeQ7VsPdYhIIukGZRoCJfxl1wqkSfLEP5lisvgcCiqYXtR6e9hvNVhsfIwJlr11NolM63rfa/mFKUVKYFVnCmWBSIjF8P5FIGwWTqs6abmiHsjGCdvcijQdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('f966ef65c1fc88deaca1d53cd4a1f963')
+# Create Rich Menu
+rich_menu_to_create = RichMenu(
+    size=RichMenuSize(width=2500, height=843),
+    selected=False,
+    name="Nice richmenu",
+    chat_bar_text="Tap here",
+    areas=[RichMenuArea(
+        bounds=RichMenuBounds(x=0, y=0, width=2500, height=843),
+        action=URIAction(label='Go to line.me', uri='https://line.me'))]
+)
+rich_menu_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+    print("headers: " + request.headers['X-Line-Signature'])
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+    print("Request body: " + body)
     # handle webhook body
     try:
         handler.handle(body, signature)
@@ -38,42 +51,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     try:
-        """
         message = TextSendMessage(text=event.message.text)
-        quick_reply = {
-				"type": "text",
-				"text": "Select your favorite food category or send me your location!",
-				"quickReply": {
-					"items": [
-						{
-							"type": "action",
-							"action": {
-										"type": "message",
-										"label": "Sushi",
-										"text": "Sushi"
-							}
-						},
-						{
-							"type": "action",
-							"action": {
-										"type": "message",
-										"label": "Tempura",
-										"text": "Tempura"
-							}
-						},
-						{
-							"type": "action",
-							"action": {
-										"type": "location",
-										"label": "Send location"
-							}
-						}
-					]
-				}
-			}
         line_bot_api.reply_message(event.reply_token, message)
-        #line_bot_api.reply_message(event.reply_token, quick_reply)
-        """
         image_url = "https://yumetwins.cdn.prismic.io/yumetwins/df97f2deda4e833a45247d07c15b0c136a57937e_465804506659e4c3d02445c894cf5bf8fdadc08a_gu_announcement_01.png"
         img = ImageSendMessage(original_content_url=image_url, preview_image_url=image_url)
         line_bot_api.reply_message(event.reply_token,img)
